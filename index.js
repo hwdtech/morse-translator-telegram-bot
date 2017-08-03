b01b9db1ddbc7569cf87454a406d354db75256a6
Maxim Malov <mmalov@hwdtech.ru>
2017-08-03 20:27:55 +0600

    Morsify

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const { Composer } = require('micro-bot');
-const morse = require('xmorse');
+const morse = require('morsify');
 const app = new Composer();
 
 app.on('text', ({ reply, message }, next) => {
   if (Math.random() > 0.5) {
     return next();
   }
-  return reply(morse.encode(message.text));
+  return reply(morse.encode(message.text, { space: ' ' }));
 });
 
 module.exports = app;
