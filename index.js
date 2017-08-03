2637bb6684ecdc6b1ee4f9d279f28ad37de2756e
Maxim Malov <mmalov@hwdtech.ru>
2017-08-03 19:44:15 +0600

    Add engines section

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Telegraf = require('telegraf');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.on('inline_query', async ({ inlineQuery, answerInlineQuery }) => {
+bot.on('inline_query', ({ inlineQuery, answerInlineQuery }) => {
   const { query } = inlineQuery;
   const result = [{
     message_test: query.toUpperCase()
