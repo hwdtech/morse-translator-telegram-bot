49ac24ef0dd5317b03600bbc24ec985e4c113865
Maxim Malov <mmalov@hwdtech.ru>
2017-08-03 19:42:16 +0600

    Microbot usage

--- index.js
+++ index.js
@@ -1,43 +1,13 @@
-'use strict';
+const Telegraf = require('telegraf');
 
-const TelegramBot = require('node-telegram-bot-api');
-const { TOKEN, APP_URL, PORT } = process.env;
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
-if (!TOKEN || !APP_URL) {
-  console.error('You must provide TOKEN and APP_URL via environment');
-  process.exit(1);
-}
-
-const options = {
-  webHook: {
-    // Port to which you should bind is assigned to $PORT variable
-    // See: https://devcenter.heroku.com/articles/dynos#local-environment-variables
-    port: PORT
-    // you do NOT need to set up certificates since Heroku provides
-    // the SSL certs already (https://<app-name>.herokuapp.com)
-    // Also no need to pass IP because on Heroku you need to bind to 0.0.0.0
-  }
-};
-
-// Heroku routes from port :443 to $PORT
-// Add URL of your app to env variable or enable Dyno Metadata
-// to get this automatically
-// See: https://devcenter.heroku.com/articles/dyno-metadata
-const bot = new TelegramBot(TOKEN, options);
-
-// This informs the Telegram servers of the new webhook.
-// Note: we do not need to pass in the cert, as it already provided
-bot.setWebHook(`${APP_URL}/bot${TOKEN}`);
-
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
-
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
-
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
+bot.on('inline_query', async ({ inlineQuery, answerInlineQuery }) => {
+  const { query } = inlineQuery;
+  const result = [{
+    message_test: query.toUpperCase()
+  }];
+  return answerInlineQuery(result);
 });
+
+bot.startPolling();
