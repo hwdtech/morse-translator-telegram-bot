6da66bc3599411c9c04cf88388bf1cca63457372
Maxim Malov <mmalov@hwdtech.ru>
2017-08-04 16:50:55 +0600

    Logging 2

--- index.js
+++ index.js
@@ -8,25 +8,24 @@ const { BOT_TOKEN, BOT_DOMAIN, PORT } = process.env;
 const app = new Telegraf(BOT_TOKEN);
 
 app.on('text', ({ replyWithAudio, message }) => {
-  console.log(`Message received: ${message.text}`);
+  console.log(`Incomming message: ${message.text}`);
   return replyWithAudio({
-    audio: `${BOT_DOMAIN}/?message=${message.text}`,
+    audio: `${BOT_DOMAIN}/morsify?message=${message.text}`,
     caption: message.slice(0, 200),
     title: message.slice(0, 10),
     reply_to_message_id: message.id
   });
 });
 
+app.telegram.getMe()
+  .then(info => console.log(`Server has initialized bot nickname. Nick: ${info.username}`))
+  .catch(err => console.error(err));
+
 const server = express();
 const webHookPath = `/tb${uuid()}`;
 server.use(app.webhookCallback(webHookPath));
 
-server.get('/ping', (req, res) => {
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('pong');
-});
-
-server.get('/', morseController);
+server.get('/morsify', morseController);
 
 server.listen(PORT, () => {
   console.log(`Bot server started at: ${BOT_DOMAIN}`);
