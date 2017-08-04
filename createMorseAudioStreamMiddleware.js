022639743d596e01c7fc8d402946fa96876a482b
Maxim Malov <mmalov@hwdtech.ru>
2017-08-04 17:35:40 +0600

    Uneascape query before morsifying

--- createMorseAudioStreamMiddleware.js
+++ createMorseAudioStreamMiddleware.js
@@ -1,3 +1,4 @@
+const qs = require('querystring');
 const morse = require('morsify');
 const createMorseAudioStream = require('./createMorseAudioStream');
 
@@ -8,7 +9,7 @@ module.exports = (req, res) => {
     return res.sendStatus(404);
   }
 
-  const encodedMessage = morse.encode(message);
+  const encodedMessage = morse.encode(qs.unescape(message));
 
   res.setHeader('Content-Type', 'audio/mpeg');
   createMorseAudioStream(encodedMessage).pipe(res);
